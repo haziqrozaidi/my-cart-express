308877f6ddf9a335c70c2383624276698d024ef4
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-05-09 00:22:33 +0800

    Add routes for home, product, and cart pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
+import ProductDetailsPage from '../pages/ProductDetailsPage.vue';
+import CartPage from '../pages/CartPage.vue';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetails',
+    component: ProductDetailsPage,
+    props: true
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: CartPage
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHistory(),
+  routes
+});
 
-export default router
+export default router;
