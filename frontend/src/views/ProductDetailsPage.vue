021ca0a0e416caa5c36768f84daa2d897b251bad
Haziq Rozaidi <182204532+haziqrozaidi@users.noreply.github.com>
2025-05-09 22:45:06 +0800

    Add product details page

---
+++ frontend/src/views/ProductDetailsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <Breadcrumb :model="breadcrumbItems" class="mb-6" />
+    <ProductDetails :productId="productId" />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import ProductDetails from '../components/ProductDetails.vue'
+import Breadcrumb from 'primevue/breadcrumb'
+
+const route = useRoute()
+const productId = computed(() => route.params.id)
+
+const breadcrumbItems = ref([
+  { label: 'Home', to: '/home' },
+  { label: 'Product Details', disabled: true },
+])
+</script>
